f424d3e4bf3d8d36477e3bb8504efa5ab77d20bd
Guilherme Freire <guitenan@hotmail.com>
2025-06-10 17:41:34 -0300

    feat: middlewares adicionados na main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers import usuarios, disciplinas, atividades, grades, login
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(usuarios.router, prefix="/usuarios")
 app.include_router(disciplinas.router, prefix="/disciplinas")
